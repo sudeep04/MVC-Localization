4afc5e4fc273506e080d68a75e7b5dc7d314c867
Sudeep Reddy <sudeep04@gmail.com>
2016-05-26 12:15:43 +0530

    Add project files.

---
+++ MVC_Localization/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Localization.MvcApplication" Language="C#" %>
---
+++ MVC_Localization/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Localization.Startup))]
+namespace MVC_Localization
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
